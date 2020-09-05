96d1e77ba2a7f994f0d4a557f2946426f5f90dd1
Bev Hatzl <61686101+bevhatzl@users.noreply.github.com>
2020-09-05 15:50:59 +1000

    display first question

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let startButton = document.querySelector("#start-button");
 let timer = document.querySelector("#timer");
+let questionGroup = document.querySelector("#question-group");
+let questionEl = document.querySelector("#question");
+let option1El = document.querySelector("#option1");
+let option2El = document.querySelector("#option2");
+let option3El = document.querySelector("#option3");
+let option4El = document.querySelector("#option4");
 
+// Creating the objects
 let q1 = new QuizQ("question1", ["choice1", "choice2", "choice3", "choice4"], "choice2");
 let q2 = new QuizQ("question1", ["choice1", "choice2", "choice3", "choice4"], "choice2");
 let q3 = new QuizQ("question1", ["choice1", "choice2", "choice3", "choice4"], "choice2");
@@ -31,6 +38,7 @@ function runQuiz() {
     // Start the timer 
     startTimer();
     // Display the questions
+    renderQuestion(newQsArray);
 
     // Check answer to question
 
@@ -42,11 +50,31 @@ function randomizeQs(array) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
+    return array;
 };
 
 function startTimer() {
-        interval = setInterval(function() {
-          secondsElapsed++;
-          timer.textContent = maxTime - secondsElapsed;
-        }, 1000);
+    interval = setInterval(function() {
+        secondsElapsed++;
+        timer.textContent = maxTime - secondsElapsed;
+    }, 1000);
+}
+
+function renderQuestion(questions) {
+    questionGroup.style.visibility = "visible"; 
+  
+    console.log(questionsArray);
+    console.log(questions);
+
+        questionEl.textContent = questions[0].question;
+        option1El.textContent = questions[0].choices[0];
+        option2El.textContent = questions[0].choices[1];
+        option3El.textContent = questions[0].choices[2];
+        option4El.textContent = questions[0].choices[3];
+
+        // Check answer to question
+
+
+  
+    
 }
